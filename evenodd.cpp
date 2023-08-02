bcc271f0c8574ddd4444437451d9dd3dd8c9fd00
Pri2ti <141238517+Pri2ti@users.noreply.github.com>
2023-08-02 17:53:36 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,22 @@
+// Even Odd
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iNo;
+    cout<<"Enter Your Number="<<endl;
+    cin>>iNo;
+    if(iNo%2==0)
+    {
+        cout<<"Number is Even";
+    }
+    else
+    {
+        cout<<"Number is Odd";
+    }
+    return 0;
+    
+
+}
+
